0f11aa221fe2172fc5f1b748befa72882902afef
das40 <oyeyioladasola30@gmail.com>
2025-02-06 14:50:21 +1100

    varaible adjustments

--- instances/variables.tf
+++ instances/variables.tf
@@ -18,14 +18,19 @@ variable "region"{
 }
 
 variable "vpc_id"{
-    default="vpc-0befa9f9ff5d3b888"
+    #default="vpc-0befa9f9ff5d3b888"
+    default="vpc-070f987b23bd64fc6"
 }
 
 variable "subnets" {
   type = list(string)
   default=[
-    "subnet-0a029f0722b30551b",
-    "subnet-0f809518c9e33b8d3",
+    #"subnet-0a029f0722b30551b",
+    #"subnet-0f809518c9e33b8d3",
+
+    "subnet-0b05da779b33b0b6f",
+    "subnet-0f09d2a870d56043d",
+
    ]
 }
 
